189e854ed544b81f12773dea9996f1da66118cf9
Teddy Wing <git@teddywing.com>
2015-04-11 16:21:26 -0400

    Create test_helper, add trial test for Board
    * Add test_helper.rb for includes
    * Add a test to sample test that initializes Board

--- test/sample_test.rb
+++ test/sample_test.rb
@@ -1,4 +1,4 @@
-require 'minitest/autorun'
+require 'test_helper'
 
 class TestSample < Minitest::Unit::TestCase
   def setup
@@ -6,5 +6,11 @@ class TestSample < Minitest::Unit::TestCase
   
   def test_sample
     assert_equal 1 + 1, 2
+  end
+end
+
+class TestBoard < Minitest::Unit::TestCase
+  def test_board
+    Board.new
   end
 end
